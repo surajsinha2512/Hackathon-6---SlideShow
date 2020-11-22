1eb50806c12f6dbd431495f4237af14cca36ccee
surajsinha2512 <61259615+surajsinha2512@users.noreply.github.com>
2020-11-22 22:53:46 +0530

    slides added

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,37 @@ import '../styles/App.css';
 
 const App = (props) => {
   console.log(props)
-  console.log(props.slides[0].title) 
-  const [value,setValue]=useState(props.title)
-  return (
+  //console.log(props.slides[0].title) 
+  const [value,setValue]=useState(props.slides[0].title)
+  const [i,seti]=useState(0)
+  const [but,setbut]=useState(0)
+
+ let j=0
+function changeNextSlides(){
+seti(i+1)
+  }
+  function changePrevSlides(){
+    seti(i-1)
+  }
+  function restartSlides(){
+    seti(0)
+  }
+
+
+  return (    
     <>
-      <div> {props.slides[0].title}</div>
+
+    {i<props.slides.length-1?<button data-testid="button-next" onClick={changeNextSlides}>Next</button>:null}
+    {i>0?<button data-testid="button-prev" onClick={changePrevSlides}>Prev</button>:null}
+    <button data-testid="button-restart" onClick={restartSlides}>Restart</button>
+    <h1 data-testid="title">{props.slides[i].title}</h1>
+    <p data-testid="text">{props.slides[i].text}</p>
+
+    
     </>
   )
 }
-
+  
 
 export default App;
+
